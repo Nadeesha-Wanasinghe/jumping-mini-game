6f29972e54181f46eeddb5947473debc373e871b
nadeesha <nadeeshalakshan525@gmail.com>
2024-02-16 00:31:27 +0530

    Add graphics and implement the checkCollision feature

--- js/main.js
+++ js/main.js
@@ -2,12 +2,28 @@ const player = document.querySelector('#player');
 const obstacle = document.querySelector('#obstacle');
 
 let isJumping = false;
+let obstacleSpeed = 3;
 
 function jump() {
     if (!isJumping) {
         isJumping = true;
         player.classList.add('jump');
 
+        function executeWithDelay(func, times, delay) {
+            let count = 0;
+          
+            function execute() {
+              if (count < times) {
+                func();
+                count++;
+                setTimeout(execute, delay);
+              }
+            }
+          
+            execute();
+        }
+          
+        executeWithDelay(renderJumpingCharacter, 11, 1000/11);
         setTimeout(() => {
             player.classList.remove('jump');
             isJumping = false;
@@ -15,7 +31,27 @@ function jump() {
     }
 }
 
-let obstacleSpeed = 3;
+function checkCollision() {
+    const playerRect = player.getBoundingClientRect();
+    const obstacleRect = obstacle.getBoundingClientRect();
+
+    if (
+        playerRect.bottom > obstacleRect.top &&
+        playerRect.right-70 > obstacleRect.left &&
+        playerRect.left +75 < obstacleRect.right
+        
+        ){
+            obstacle.style.animation = "none";
+            endGame();
+        }
+    }
+    
+function endGame() {
+    alert('Game Over!');
+    let obstacleSpeed = 3;
+    obstacle.style.right = '0px';
+    obstacle.style.animation = "rotateAnimation 1s infinite linear";
+}
 
 function moveObstacle() {
     const currentPosition = parseInt(getComputedStyle(obstacle).right);
@@ -26,18 +62,42 @@ function moveObstacle() {
     } else {
         obstacle.style.right = newPosition + 'px';
     }
+
+    checkCollision();
 }
 
 document.addEventListener('click', (e)=> {
     jump();
-});
+  });
 
 document.addEventListener('keydown',(e)=>{
     if (e.code === 'Space' || e.code === 'ArrowUp' || e.code === 'Enter') {
       jump();
     }
-});
+  });
 
 setInterval(moveObstacle, 0.5);
 
+let indexJump = 1;
+function renderJumpingCharacter(){
+    if (isJumping){
+        if (indexJump >= 12) indexJump = 1;
+        player.style.backgroundImage = 
+            `url('img/character/Jump (${indexJump++}).png')`;
+    }
+}
+
+let indexRun = 1;
+function renderRunCharacter(){
+
+    if (indexRun >= 9) indexRun = 1;
+    player.style.backgroundImage = 
+        `url('img/character/Run (${indexRun++}).png')`;
+    
+}
+
+setInterval(()=>{
+    if(!isJumping){
+        renderRunCharacter();
+    }},70); 
 
